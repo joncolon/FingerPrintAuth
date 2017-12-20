f37a5869d1ddd15045945b1a210c5753d7047dae
joncolon <jonatron2.0@gmail.com>
2017-12-19 23:03:33 -0500

    Initial commit

---
+++ app/src/main/java/com/tronography/fingerprintauth/FingerPrintAuth.kt
@@ -0,0 +1,12 @@
+package com.tronography.fingerprintauth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FingerPrintAuth : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fingerprint_auth)
+    }
+}
